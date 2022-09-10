76f0a9ca6200a4bd112f6fc78244154e195bacaa
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-09-10 09:26:44 -0500

    feat: add game menu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace StrangleGame
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string menuOption = "";
+
+      do
+      {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("============ AHORCADO EN C# ============");
+        Console.ForegroundColor = ConsoleColor.White;
+
+        Console.WriteLine("");
+        Console.WriteLine("Opciones:");
+        Console.WriteLine("1. Jugar partida");
+        Console.WriteLine("2. Información del autor");
+        Console.WriteLine("3. Salir");
+        Console.WriteLine("");
+
+        Console.Write("Seleccione una opción: ");
+        menuOption = Console.ReadLine() ?? string.Empty;
+        Console.WriteLine("");
+
+        switch (menuOption)
+        {
+          case "1":
+            Console.WriteLine("Jugar partida");
+            break;
+
+          case "2":
+            Console.WriteLine("Ángel Cruz (langelcruzlara@gmail.com)");
+            Console.WriteLine("Software Developer");
+            Console.WriteLine("GitHub: https://github.com/AngelCruzL");
+            break;
+
+          case "3":
+            Console.WriteLine("Saliendo del juego...");
+            break;
+
+          default:
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Opción no válida\n");
+            Console.ForegroundColor = ConsoleColor.White;
+            menuOption = "--";
+            break;
+        }
+      } while (menuOption == "--");
+    }
+  }
+}
